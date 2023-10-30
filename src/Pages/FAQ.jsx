d880f3930751289d778cbf19599a597260225226
Sumit Panchal <112932064+Sumitpanchal1993@users.noreply.github.com>
2023-10-30 19:10:52 +0530

    faq page made and done

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -1,23 +1,31 @@
 import React from "react"
+import './FAQ.css'
+import faq_data from "../Static Data/FAQ_data"
 
 export default function FAQ() {
   return (
     <>
-      <div>
-        <h1>HELP CENTER</h1>
-      </div>
-      <div>
-        <h1>If You are facing any issue you are at right place</h1>
-      </div>
-      <div>
+      <div className='FAQ_base'>
+        <div className='title'>
+          <h1>Thanks for Visiting the Helpdesk</h1>
+          <h2>FAQ</h2>
+        </div>
+        <div className='faq_srch'>
+          <span><input type='text' /><button>Search</button> </span>
+        </div>
+        <hr />
+
+
+        {faq_data.map((item) => {
+          return (
+            <div className='QA'>
+              <h3>{item.que}</h3>
+              <p>{item.ans}</p>
+            </div>
+          )
+        })}
         <div>
-          <input type='text' placeHolder='Search Here' />
-          <button>Search</button>
-        </div>          
-      </div>
-      <hr/>
-      <div >
-          Main Content
+        </div>
       </div>
     </>
   )
